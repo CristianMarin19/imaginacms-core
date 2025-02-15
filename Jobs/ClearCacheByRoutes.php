4d4d841eb4b887ba3713811facac558677f02afa
msolanorenuity <msolano@contractor.renuityhome.com>
2025-02-15 14:22:41 -0500

    added queue name `ClearCacheWithCDN` to HasCach

--- Jobs/ClearCacheByRoutes.php
+++ Jobs/ClearCacheByRoutes.php
@@ -12,38 +12,44 @@ use Modules\Iwebhooks\Entities\Log;
 class ClearCacheByRoutes implements ShouldQueue
 {
   use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-  
+
   public $urls;
   public $entity;
-  
+
   /**
    * Create a new job instance.
    */
   public function __construct($entity = null, $urls = [])
   {
     $this->entity = $entity;
-    
-    if(isset($this->entity->id))
+
+    if (isset($this->entity->id))
       $this->urls = $this->initCacheClearableData('urls');
-    
+
     !is_array($urls) ? $urls = [$urls] : false;
     $this->urls = array_merge($this->urls ?? [], $urls);
   }
-  
+
   /**
    * Execute the job.
    */
   public function handle()
   {
     $client = new \GuzzleHttp\Client();
-    if (!empty($this->urls)){
+    if (!empty($this->urls)) {
       foreach ($this->urls as $url) {
-        $promise = $client->get($url, ['headers' => ['icache-bypass' => 1]]);
-        \Log::info('Route Update Cache: '. $url);
+        try {
+          \Log::info('CACHE::RUNING ' . $url);
+          $promise = $client->get($url, ['headers' => ['icache-bypass' => 1]]);
+          \Log::info('CACHE::DONE ' . $url);
+        }catch (\Exception $e){
+          \Log::info('CACHE::FAILED' . ($url ?? 'no url'). ' --> ' . $e->getMessage());
+        }
+
       }
     }
   }
-  
+
   /**
    * Return the needed data by cache provider from model
    *
@@ -56,6 +62,12 @@ class ClearCacheByRoutes implements ShouldQueue
     if (method_exists($this->entity, 'getCacheClearableData')) {
       $cacheClearableData = $this->entity->getCacheClearableData();
       $response = $cacheClearableData[$type] ?? null;
+      $baseUrl = config("app.url");
+      //Move the base url to the end (home is expencive to load time)
+      if (($baseUrlKey = array_search($baseUrl, $response)) !== false) {
+        unset($response[$baseUrlKey]);
+        $response[] = $baseUrl;
+      }
     }
     return $response;
   }
